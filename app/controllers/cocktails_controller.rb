66c2d804fcb518dfade4f59222734ff5e81b0198
Max Philips <maximilien.philips@gmail.com>
2016-08-18 15:59:46 +0100

    Cocktail cards with name

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,30 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = @cocktail.build(cocktail_params)
+    @cocktail.save
+  end
+
+  private
+
+  def cocktail_param
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,29 @@
 class DosesController < ApplicationController
+
+  before_action :set_cocktail, only: [:new, :create,]
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = @cocktail.doses.build(dose_params)
+    @dose.save
+  end
+
+  def destroy
+    @dose = Dose.find(params[:cocktail_id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_param
+    params.require(:dose).permit(:description, :cocktail)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
 end
